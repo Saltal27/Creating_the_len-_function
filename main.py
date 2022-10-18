f7ff0432ef57543342c0b783a73126acc750434e
Sal Tal <110280707+Saltal27@users.noreply.github.com>
2022-10-18 20:59:29 +0000

    quick update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def len(string):
+def LEN(string):
   number_of_letters = 0
   for letter in string:
     number_of_letters += 1
   return number_of_letters
 
-print(len("Omar"))
+print(LEN("Omar"))
